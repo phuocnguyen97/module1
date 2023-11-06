81fa1fb5d742bc094eb3cf7c959af408f68ae3cd
phuocnguyen97 <sanhphuoc.pnt@gmail.com>
2023-11-06 22:45:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = 6;
+let b = 10;
+let c = a + b;
+console.log("Tổng là: ", c);
+
+// alert("Tổng là: " + c);
+
+// prompt("Nhập số: ");
+
+// confirm("Bạn có muốn nhập con số này không?");
+
+// in trên trang web
+// document.write("Tổng là: " + c);
